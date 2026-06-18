716925bf34499ea38845e257a5abc94cbba35821
Rafli <achmadraflitp@gmail.com>
2026-06-18 11:04:11 +0700

    Update .gitignore to include local environment files and refactor supabase.js to use environment variables for configuration

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 // Cukup gunakan URL dasar project
-const supabaseUrl = 'https://nqxynocecuhwnzslaplc.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xeHlub2NlY3Vod256c2xhcGxjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1NjU0NTEsImV4cCI6MjA5NzE0MTQ1MX0.Alc58yU3OzSLsV5GIIt5co5ivltRRsmJ8xPuMtOFCa8'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
